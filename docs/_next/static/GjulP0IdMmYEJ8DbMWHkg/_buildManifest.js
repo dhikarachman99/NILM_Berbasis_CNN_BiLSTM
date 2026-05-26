28743b786e355f49ea47fc0fcde9e51347e2c16b
dhikarachman99 <191452565+dhikarachman99@users.noreply.github.com>
2026-05-26 13:04:17 +0000

    deploy: update GitHub Pages dashboard

---
+++ docs/_next/static/GjulP0IdMmYEJ8DbMWHkg/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/NILM_Berbasis_CNN_BiLSTM//_next/:path+",
+        "destination": "/NILM_Berbasis_CNN_BiLSTM/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
